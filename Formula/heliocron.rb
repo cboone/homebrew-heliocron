31f7d3a908a2fc64fa1134e381bbfdce82168930
Chris Boone <cboone@users.noreply.github.com>
2025-12-31 18:39:10 -0500

    fix: remove deprecated bottle block

--- Formula/heliocron.rb
+++ Formula/heliocron.rb
@@ -12,12 +12,6 @@ class Heliocron < Formula
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 
-  bottle do
-    root_url "https://github.com/cboone/homebrew-heliocron/raw/master/bottles"
-    cellar :any_skip_relocation
-    sha256 "7ebacca37bacc0bbbc81b950bce63637c8ec08741819179d76da8e55c059d2c9" => :catalina
-  end
-
   test do
     assert_match "sunset: 2020-05-01 20:00:26 -07:00", pipe_output(
       "#{bin}/heliocron --latitude 37.783N --longitude 122.417W --date 2020-05-01 report",
